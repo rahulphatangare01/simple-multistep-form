2b590f47bdba88ed2570b413aa0ecfe86f3aadc7
Rahul phatangare <rahulphatangare01@gmail.com>
2024-06-19 22:07:22 +0530

    redux-store configure

---
+++ src/components/personalInformation/PersonalInformation.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const PersonalInformation = () => {
+  return (
+    <div>
+      Personal Information
+
+      <form>
+        <label>Name</label>
+        <input  placeholder='Enter Name' />
+
+        <label>Name</label>
+        <input  placeholder='Enter Name' />
+
+        <label>Name</label>
+        <input  placeholder='Enter Name' />
+
+        <label>Name</label>
+        <input  placeholder='Enter Name' />
+
+        <button type='submit' > Submit </button>
+ 
+        
+      </form>
+    </div>
+  )
+}
+
+export default PersonalInformation
---
+++ src/store/slices/personalInformation.js
@@ -0,0 +1,8 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+
+export const personalData = createSlice({
+   name:"personalData",
+   initialState: [],
+   
+})
